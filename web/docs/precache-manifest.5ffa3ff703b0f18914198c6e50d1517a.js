e7e3daacfc36525f35373d5ddaf67e5638eecf9e
unknown <trevorsm@pm.me>
2020-12-10 15:17:19 -0700

    removed jest & mit build

---
+++ web/docs/precache-manifest.5ffa3ff703b0f18914198c6e50d1517a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06599cc7ad93ff3c54ae705fcf6d218b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e83bec51bd621c360a32",
+    "url": "/static/js/2.0a3e5101.chunk.js"
+  },
+  {
+    "revision": "db57016e803849b0e5c2",
+    "url": "/static/js/main.897aa648.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
